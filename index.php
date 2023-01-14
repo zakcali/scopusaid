ed0d97d6741cfcf2fc63cfc5f7a9516838a00031
Zafer Akçalı <zaferakcali@yahoo.com>
2023-01-14 08:42:56 +0300

    added h-index, enhanced view

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<!-- scopusaid V2.4: bu yazılım Dr. Zafer Akçalı tarafından oluşturulmuştur 
-programmed by Zafer Akçalı, MD -->
+<!-- scopusaid V2.5: bu yazılım Dr. Zafer Akçalı tarafından oluşturulmuştur 
+programmed by Zafer Akçalı, MD added: enhanced view, h-index, changed name, surname position in array-->
 <html>
 <script src="https://cdn.jsdelivr.net/g/filesaver.js"></script> 
 <head>
@@ -34,8 +34,11 @@ Scopus aid  giriniz. <?php echo ' '.$sa->dikkat;?><br/>
 Ad: <input type="text" name="ad" size="16"  id="ad" value="<?php echo $sa->ad;?>"> 
 Soyad: <input type="text" name="soyad" size="20"  id="soyad" value="<?php echo $sa->soyad;?>"> 
 ORCID: <input type="text" name="ORCID" size="20"  id="ORCID" value="<?php echo $sa->authorOrcid;?>"><br>
-AuthorId: <input type="text" name="said" size="12"  id="said" value="<?php echo $sa->authorId;?>"> 
-Yayın sayısı: <input type="text" name="sayi" size="6"  id="sayi" value="<?php echo $sa->sayi;?>"> <br>
+AuthorId: <input type="text" name="said" size="12"  id="said" value="<?php echo $sa->authorId;?>">  
+h-index: <input type="text" name="hindex" size="2"  id="hindex" value="<?php echo $sa->hindex;?>"> 
+yayın sayısı: <input type="text" name="yayins" size="4"  id="yayins" value="<?php echo $sa->yayinS;?>"> 
+atif sayısı: <input type="text" name="atifs" size="4"  id="atifs" value="<?php echo $sa->atifS;?>"> <br>
+Listelenen yayın sayısı: <input type="text" name="sayi" size="6"  id="sayi" value="<?php echo $sa->sayi;?>">
 Yayınlar<br>
 <textarea rows = "20" cols = "90" name = "yayinlar"  wrap="off" id="yayinlar"><?php echo $sa->yayinlar;?></textarea>  <br>
 <script>
